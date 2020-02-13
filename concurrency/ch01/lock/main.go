d80911794f5a1f5227acb989a97579a56dd72c71
Keith Noguchi <keith.noguchi@gmail.com>
2020-02-12 23:39:54 -0800

    concurrency/ch01/dead: Deadlock example

---
+++ concurrency/ch01/lock/main.go
@@ -0,0 +1,24 @@
+// Memory Access Synchronization, page 8
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var memoryAccess sync.Mutex
+	var value int
+	go func() {
+		memoryAccess.Lock()
+		value++
+		memoryAccess.Unlock()
+	}()
+	memoryAccess.Lock()
+	if value == 0 {
+		fmt.Printf("the value is %v.\n", value)
+	} else {
+		fmt.Printf("the value is %v.\n", value)
+	}
+	memoryAccess.Unlock()
+}
