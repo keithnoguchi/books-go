1b6df795da6eb4775e191a4844f743fa208d1b8e
Keith Noguchi <keith.noguchi@gmail.com>
2020-04-21 16:01:25 -0700

    distributed/ch03: Add log package

---
+++ distributed/ch03/store.go
@@ -0,0 +1,69 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	lock sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+func (s *store) Append(p []byte) (uint64, uint64, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	pos := s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) ReadAt(pos uint64) ([]byte, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
--- distributed/go.mod
+++ distributed/go.mod
@@ -3,4 +3,8 @@ module distributed
 go 1.14
 
 require github.com/gorilla/mux v1.7.4
-require github.com/gogo/protobuf v1.3.1
+
+require (
+	github.com/gogo/protobuf v1.3.1
+	github.com/stretchr/testify v1.5.1
+)
