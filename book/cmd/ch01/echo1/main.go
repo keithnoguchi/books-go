c200cbb042854cab08451098e576ff45ad6dd427
Keith Noguchi <keith.noguchi@gmail.com>
2021-02-06 17:55:51 -0800

    book/cmd/ch01/echo1: Print out the command line arguments

---
+++ book/cmd/ch01/echo1/main.go
@@ -0,0 +1,16 @@
+// Print out the command line arguments
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
