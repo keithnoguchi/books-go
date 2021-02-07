38e9c7f70fbe9837f36c4300c3c878a302df0386
Keith Noguchi <keith.noguchi@gmail.com>
2021-02-06 18:05:16 -0800

    book/cmd/ch01/echo2: range example

---
+++ book/cmd/ch01/echo2/main.go
@@ -0,0 +1,16 @@
+// echo2: range example
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
