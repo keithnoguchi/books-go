1d566c92d89d4d8f3aa1d3517f3968e38576ac40
Keith Noguchi <keith.noguchi@gmail.com>
2021-08-22 13:18:09 -0700

    book/cmd/flag: Add fahrenheit support for -temp cmdline argument

--- book/ch07/celcious.go
+++ book/ch07/celcious.go
@@ -3,12 +3,24 @@ package ch07
 import (
 	"flag"
 	"fmt"
+	"strings"
 )
 
 type Celcious float64
 
+
 func (c Celcious) String() string {
 	return fmt.Sprintf("%.05fC", c)
+}
+
+type Fahrenheit float64
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%.05F", f)
+}
+
+func ftoc(f Fahrenheit) Celcious {
+	return Celcious((f - 32) * 5 / 9)
 }
 
 func CelciousFlag(name string, val Celcious, usage string) *Celcious {
@@ -23,9 +35,12 @@ func (f *celciousFlag) Set(s string) error {
 	var unit string
 	var val float64
 	fmt.Sscanf(s, "%f%s", &val, &unit)
-	switch unit {
-	case "C", "c":
+	switch strings.ToUpper(unit) {
+	case "C":
 		f.Celcious = Celcious(val)
+		return nil
+	case "F":
+		f.Celcious = ftoc(Fahrenheit(val))
 		return nil
 	}
 	return fmt.Errorf("invalid temperature: %q", s)
